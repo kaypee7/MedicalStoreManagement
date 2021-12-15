dc277ac5c28393df0db6e45445a518878e8e5f1b
Kailash Patel <kailashpatel.kp779@gmail.com>
2021-12-15 14:49:52 +0530

    Medicine page update

--- MedicalStoreManagement/DjangoMedicalApp/views.py
+++ MedicalStoreManagement/DjangoMedicalApp/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework.generics import get_object_or_404
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticated
-from .serializers import CompanySerializer
+from .serializers import CompanyBankSerializer, CompanySerializer, MedicineSerializer
 
-from .models import Company
+from .models import Company, CompanyBank, Medicine
 
 # Create your views here.
 class CompanyViewSet(viewsets.ViewSet):
@@ -21,7 +22,7 @@ class CompanyViewSet(viewsets.ViewSet):
     def create(self,request):
         try:
             serializer=CompanySerializer(data=request.data,context={"request":request})
-            serializer.is_valid()
+            serializer.is_valid(raise_exception=True)
             serializer.save()
             dict_response={"error":False,"message":"Company Data Save Successfully"}
         except:
@@ -33,7 +34,7 @@ class CompanyViewSet(viewsets.ViewSet):
             queryset=Company.objects.all()
             company=get_object_or_404(queryset,pk=pk)
             serializer=CompanySerializer(company,data=request.data,context={"request":request})
-            serializer.is_valid()
+            serializer.is_valid(raise_exception=True)
             serializer.save()
             dict_response={"error":False,"message":"Successfully Updated Company Data"}
 
@@ -41,6 +42,83 @@ class CompanyViewSet(viewsets.ViewSet):
             dict_response={"error":True,"message":"Error During Updating Company Data"}
         
         return Response(dict_response)
+
+
+class CompanyBankViewSet(viewsets.ViewSet):
+    authentication_classes=[JWTAuthentication]
+    permission_classes=[IsAuthenticated]
+
+    def create(self,request):
+        try:
+            serializer=CompanyBankSerializer(data=request.data,context={"request":request})
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            dict_response={"error":False,"message":"Company Bank Data Save Successfully"}
+        except:
+            dict_response={"error":True,"message":"Error During Saving Company Bank Data"}
+        return Response(dict_response)
+
+    def list(self,request):
+        companybank=CompanyBank.objects.all()
+        serializer=CompanyBankSerializer(companybank, many=True, context={"request":request})
+        response_dict= {"error":False,"message":"All Company Bank List Data","data":serializer.data}
+        return Response(response_dict)
+
+    def retrieve(self,request,pk=None):
+        queryset=CompanyBank.objects.all()
+        companybank=get_object_or_404(queryset,pk=pk)
+        serializer=CompanyBankSerializer(companybank,context={"request":request})
+        return Response({"error":False,"message":"Single Data Fetch","data":serializer.data})
+
+    def update(self,request,pk=None):
+        queryset=CompanyBank.objects.all()
+        companybank=get_object_or_404(queryset,pk=pk)
+        serializer=CompanyBankSerializer(companybank,data=request.data,context={"request":request})
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({"error":False,"message":"Data Has Been Updated"})
+
+class CompanyNameViewSet(generics.ListAPIView):
+    serializer_class= CompanySerializer
+    def get_queryset(self):
+        name=self.kwargs["name"]
+        return Company.objects.filter(name=name)
+
+
+class MedicineViewSet(viewsets.ViewSet):
+    authentication_classes=[JWTAuthentication]
+    permission_classes=[IsAuthenticated]
+
+    def create(self,request):
+        try:
+            serializer=MedicineSerializer(data=request.data,context={"request":request})
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            dict_response={"error":False,"message":"Medicine Data Save Successfully"}
+        except:
+            dict_response={"error":True,"message":"Error During Saving Medicine Data"}
+        return Response(dict_response)
+
+    def list(self,request):
+        medicine=Medicine.objects.all()
+        serializer=MedicineSerializer(medicine, many=True, context={"request":request})
+        response_dict= {"error":False,"message":"All Medicine List Data","data":serializer.data}
+        return Response(response_dict)
+
+    def retrieve(self,request,pk=None):
+        queryset=Medicine.objects.all()
+        medicine=get_object_or_404(queryset,pk=pk)
+        serializer=MedicineSerializer(medicine,context={"request":request})
+        return Response({"error":False,"message":"Single Data Fetch","data":serializer.data})
+
+    def update(self,request,pk=None):
+        queryset=Medicine.objects.all()
+        medicine=get_object_or_404(queryset,pk=pk)
+        serializer=MedicineSerializer(medicine,data=request.data,context={"request":request})
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({"error":False,"message":"Data Has Been Updated"})
+
 
 
 company_list=CompanyViewSet.as_view({"get":"list"})
--- MedicalStoreManagement/MedicalStoreManagement/urls.py
+++ MedicalStoreManagement/MedicalStoreManagement/urls.py
@@ -6,10 +6,14 @@ from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
 
 router=routers.DefaultRouter()
 router.register("company", views.CompanyViewSet,basename="company")
+router.register("companybank", views.CompanyBankViewSet,basename="companybank")
+router.register("medicine", views.MedicineViewSet,basename="medicine")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/',include(router.urls)),
     path('api/gettoken/',TokenObtainPairView.as_view(),name="gettoken"),
-    path('api/resfresh_token/',TokenRefreshView.as_view(),name="resfresh_token")
+    path('api/resfresh_token/',TokenRefreshView.as_view(),name="resfresh_token"),
+    path('api/companybyname/<str:name>',views.CompanyNameViewSet.as_view(),name="companybyname"),
+
 ]
